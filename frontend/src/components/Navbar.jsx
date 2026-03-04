fa3df2ac54c02a836ac13ad788a05660c0822cec
Arshdeep Singh <arshdeepchhabra1712@gmail.com>
2026-03-04 20:52:55 +0530

    Ui done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { assets } from '../assets/assets'
 
 const Navbar = () => {
   return (
+    <>
     <div className='w-full flex justify-between items-center font-semibold'>
         <div className='flex items-center gap-2'>
             <img src={assets.arrow_left} className='w-8 bg-black p-2 rounded-2xl cursor-pointer' alt="" />
@@ -11,9 +12,15 @@ const Navbar = () => {
         <div className='flex items-center gap-4'>
             <p className='bg-white text-black text-[15px] rounded-2xl hidden md:block cursor-pointer px-4 py-1'>Explore Premium</p>
             <p className='bg-black px-3 py-1 rounded-2xl text-[15px] cursor-pointer'>Install App</p>
-            <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center'>D</p>
+            <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center cursor-pointer'>D</p>
         </div>
     </div>
+    <div className='flex items-center gap-2 mt-4'>
+            <p className='px-4 py-1 bg-white text-black rounded-2xl cursor-pointer'>All</p>
+            <p className='px-4 py-1 bg-black text-white rounded-2xl cursor-pointer'>Music</p>
+            <p className='px-4 py-1 bg-black text-white rounded-2xl cursor-pointer'>Podcast</p>
+    </div>
+    </>
   )
 }
 
