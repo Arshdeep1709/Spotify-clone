d1753b528efbb1b3210e9c8b7a909d9fe2c3b091
Arshdeep Singh <arshdeepchhabra1712@gmail.com>
2026-03-05 21:13:41 +0530

    updated UI

--- frontend/src/components/DisplayAlbum.jsx
+++ frontend/src/components/DisplayAlbum.jsx
@@ -40,6 +40,9 @@ const DisplayAlbum = () => {
             <img src={item.image} className='inline w-10 mr-5' alt="" />
             {item.name}
           </p>
+          <p className='text-[15px]'>{albumData.name}</p>
+          <p className='text-[15px] hidden sm:block'>5 days ago</p>
+          <p className='text-[15px] text-center'>{item.duration}</p>
         </div>
       ))
     }   
